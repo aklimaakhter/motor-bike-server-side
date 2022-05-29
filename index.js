94f0c0351ccb1d8aef77233edcc6c050579b4ceb
aklimaakhter <aklimawebd@gmail.com>
2022-05-29 20:11:23 +0600

    admin part added

--- index.js
+++ index.js
@@ -48,14 +48,29 @@ async function run(){
      res.send(users);
    })
 
+   app.get('/admin/:email', async(req, res)=>{
+     const email = req.params.email;
+     const user = await userCollection.findOne({email: email});
+     const isAdmin = user.role === 'admin';
+     
+   })
+
    app.put('/user/admin/:email', verifyJWT, async(req, res) =>{
-       const email = req.params.email;      
-       const filter = {email: email};
-     const updateDoc = {
-       $set: {role: 'admin'},   
-     };
-     const result = await userCollection.updateOne(filter, updateDoc);
-     res.send(result);
+       const email = req.params.email; 
+       const requester = req.decoded.email;
+       const requesterAccount = await userCollection.findOne({email: requester});
+       if(requesterAccount.role === 'admin'){
+         const filter = { email: email };
+         const updateDoc = {
+           $set: { role: 'admin' },
+         };
+         const result = await userCollection.updateOne(filter, updateDoc);
+         res.send(result);
+       }
+       else{
+         res.status(403).send({})
+       }
+       
    })
 
    app.put('/user/:email', async(req, res) =>{
